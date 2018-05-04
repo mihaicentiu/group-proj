7cf7cbc1546375c390f0285c061f6c8deacfff69
Mihai <mihaicentiu@gmail.com>
2018-05-04 14:16:41 +0100

    Connection status

---
+++ Assets/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Uniduino;
+
+public class Connection : MonoBehaviour {
+
+	public Text ConnectionText;
+	public Arduino arduino;
+	private GameObject ArduinoScript;
+
+	// Use this for initialization
+	void Start () {
+		arduino = Arduino.global;
+		ArduinoScript = GameObject.Find ("Uniduino");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ArduinoScript.GetComponent<Arduino> ().Connected) {
+			ConnectionText.gameObject.SetActive (true);
+		}
+	}
+}
